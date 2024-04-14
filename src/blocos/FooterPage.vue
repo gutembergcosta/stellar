e6ab3ed73d9abf61e44fbbe9ba0c9be1ea9763d7
gutemberg <gutemberg.costa@loginfo.com.br>
2024-04-14 14:43:08 -0300

    footer

--- src/blocos/FooterPage.vue
+++ src/blocos/FooterPage.vue
@@ -1,9 +1,15 @@
 <template>
 	<footer class="">
-		
-		<div classx="d-sm-flex justify-content-center justify-content-sm-between">
-			<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+		<div class="d-sm-flex justify-content-center justify-content-sm-between p-4">
+			<span class="text-muted d-block text-center text-sm-left d-sm-inline-block"></span>
+			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Painel de controle | 2024</span>
 		</div>
 	</footer>
 </template>
+
+<style scoped>
+	footer{
+		background: white;
+		border-top: 1px solid #eee;
+	}
+</style>
