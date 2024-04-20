ac3e23c165b75819ef528d06723697b1d72b6e83
gutemberg <gutemberg.costa@loginfo.com.br>
2024-04-20 07:44:59 -0300

    clear arquivo

--- php/includes/titular.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-    <h3 class="page-title"> Form elements </h3>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Forms</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Form elements</li>
-        </ol>
-    </nav>
-</div>
