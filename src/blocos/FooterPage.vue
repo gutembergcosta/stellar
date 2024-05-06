c4a4ef3c420aab7523792ccb2e3572556d8e54cd
gutemberg <gutemberg.costa@loginfo.com.br>
2024-05-06 12:12:16 -0300

    upd: rodapé

--- src/blocos/FooterPage.vue
+++ src/blocos/FooterPage.vue
@@ -2,7 +2,7 @@
 	<footer class="">
 		<div class="d-sm-flex justify-content-center justify-content-sm-between p-4">
 			<span class="text-muted d-block text-center text-sm-left d-sm-inline-block"></span>
-			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Painel de controle | 2024</span>
+			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><i class="fa-solid fa-cog"></i> Painel de controle | 2024</span>
 		</div>
 	</footer>
 </template>
